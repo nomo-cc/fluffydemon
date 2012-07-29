72a91760071591fe58a8e9ff8c3414071db866c0
nmo <github@nomo.cc>
2012-07-29 14:53:53 +0200

    initial release
    known bugs: doesn't handle level 2 subdirectories correctly (for example
    Lupin III)

---
+++ MainForm.Designer.cs
@@ -0,0 +1,216 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: nomo
+ * Date: 29.05.2012
+ * Time: 18:43
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace fluffydemon
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(10, 66);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(131, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Load category";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// checkedListBox1
+			// 
+			this.checkedListBox1.CheckOnClick = true;
+			this.checkedListBox1.FormattingEnabled = true;
+			this.checkedListBox1.Location = new System.Drawing.Point(6, 22);
+			this.checkedListBox1.Name = "checkedListBox1";
+			this.checkedListBox1.Size = new System.Drawing.Size(278, 139);
+			this.checkedListBox1.TabIndex = 2;
+			// 
+			// button2
+			// 
+			this.button2.Enabled = false;
+			this.button2.Location = new System.Drawing.Point(290, 118);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(74, 44);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Download animes";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(10, 39);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(131, 21);
+			this.comboBox1.TabIndex = 4;
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(6, 97);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(358, 23);
+			this.progressBar1.TabIndex = 5;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::fluffydemon.Resource1.konata_1;
+			this.pictureBox1.InitialImage = null;
+			this.pictureBox1.Location = new System.Drawing.Point(6, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(200, 132);
+			this.pictureBox1.TabIndex = 6;
+			this.pictureBox1.TabStop = false;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.comboBox1);
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Location = new System.Drawing.Point(229, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(147, 132);
+			this.groupBox1.TabIndex = 7;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "1: select the category...";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.checkedListBox1);
+			this.groupBox2.Controls.Add(this.button2);
+			this.groupBox2.Location = new System.Drawing.Point(6, 150);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(370, 173);
+			this.groupBox2.TabIndex = 8;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "2. Select the animes you want...";
+			this.groupBox2.Visible = false;
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.label4);
+			this.groupBox3.Controls.Add(this.label3);
+			this.groupBox3.Controls.Add(this.label2);
+			this.groupBox3.Controls.Add(this.label1);
+			this.groupBox3.Controls.Add(this.progressBar1);
+			this.groupBox3.Location = new System.Drawing.Point(6, 329);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(370, 126);
+			this.groupBox3.TabIndex = 9;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "3. Sit back and relax";
+			this.groupBox3.Visible = false;
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(6, 77);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(352, 18);
+			this.label4.TabIndex = 9;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(6, 59);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(352, 18);
+			this.label3.TabIndex = 8;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(6, 41);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(352, 18);
+			this.label2.TabIndex = 7;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(6, 23);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(352, 18);
+			this.label1.TabIndex = 6;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(382, 456);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.pictureBox1);
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(390, 483);
+			this.MinimumSize = new System.Drawing.Size(390, 483);
+			this.Name = "MainForm";
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			this.Text = "fluffydemon";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainFormFormClosing);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox3.ResumeLayout(false);
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.CheckedListBox checkedListBox1;
+		private System.Windows.Forms.Button button1;
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,497 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: nomo
+ * Date: 29.05.2012
+ * Time: 18:43
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net;
+using System.Xml;
+using System.Threading;
+using System.IO;
+using System.Globalization;
+using System.ComponentModel;
+//using System.Threading;
+
+namespace fluffydemon
+{
+	
+	class userData
+
+	{
+
+		private string _value;
+		private string _name;
+
+		public string Value
+		{
+
+			get { return _value; }set { _value = value; }
+		}
+
+		public string Name
+		{
+
+			get { return _name; }
+
+			set { _name = value; }
+		}
+
+		public userData(string name, string value)
+		{
+
+			_name = name;
+
+			_value = value;
+
+		}
+
+		public override string ToString()
+		{
+
+			return _name;
+		}
+
+	}
+	
+	
+	
+	
+	
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		
+		
+		
+		public int downloadfile(string d_url, string d_file)
+		{
+			try
+			{
+				using (WebClient Client = new WebClient ())
+				{
+					Client.DownloadFile(d_url, d_file);
+				}
+				return 0;
+			}
+			catch(Exception e)
+			{
+				statout("Exception in downloadfile "+d_url+" "+e);
+				return -1;
+			}
+		}
+		
+		
+		public  void statout(string towrite)
+		{
+			try
+			{
+				//	textBox1.AppendText(towrite+"\r\n");
+				MessageBox.Show(towrite);
+			}
+			catch
+			{
+				
+			}
+		}
+		
+		public void parseList(string lpath)
+		{
+			bool inlist = false;
+			
+			_items.Clear();
+			
+			try
+			{
+
+				using (StreamReader sr = new StreamReader(lpath))
+				{
+					String line;
+					// Read and display lines from the file until the end of
+					// the file is reached.
+					while ((line = sr.ReadLine()) != null)
+					{
+						if(line.IndexOf( "folder-list") != -1)inlist=true;
+						if(line.IndexOf("rightbanner")!= -1)break;
+						
+						if(inlist==true){
+							
+							if(line.IndexOf("title=")!= -1)
+							{
+								if(line.IndexOf("Folder Back")== -1)
+									getfolderpath(line);
+								//	statout(line);
+							}
+						}
+					}
+					checkedListBox1.DataSource = null;
+					checkedListBox1.DataSource = _items;
+				}
+			}
+			catch (Exception e)
+			{
+				// Let the user know what went wrong.
+				statout("The file could not be read:");
+				statout(e.Message);
+			}
+			
+			
+		}
+		
+		List<string> _items = new List<string>();
+		
+		public void getfolderpath(string theline)
+		{
+			string[] substrs = theline.Split('\"');
+			//	statout(substrs[1]+" - "+substrs[3]);
+			
+			_items.Add(substrs[1]);
+			
+		}
+		
+		
+		
+		public void parsefolder(string furl, bool insub)
+		{
+			
+			//	if(activesubfolder=="")	parsesubfolder(furl);
+			
+			
+			downloadfile(furl, "list2.html");
+			
+			bool hassubfolder = true;
+			
+			
+			
+			try
+			{
+
+				using (StreamReader sr = new StreamReader("list2.html"))
+				{
+					String line;
+					// Read and display lines from the file until the end of
+					// the file is reached.
+					while ((line = sr.ReadLine()) != null)
+					{
+						if(line.IndexOf( "Click to go to the download page") != -1)
+						{
+							hassubfolder = false;
+							string[] substrs = line.Split('\"');
+							
+							downloadfolder(substrs[1]);
+							
+							activesubfolder="";
+							
+							//	statout(line);
+							break;
+						}
+						
+					}
+					
+					
+					
+				}
+				
+				//	if(hassubfolder)
+				//	{
+				if(insub == false)parsesubfolder(furl);
+				
+				//	}
+			}
+			catch (Exception e)
+			{
+				// Let the user know what went wrong.
+				statout("The file could not be read:");
+				statout(e.Message);
+			}
+			
+			
+			
+			
+		}
+		
+		
+		void parsesubfolder(string furl)
+		{
+			downloadfile(furl, "list2_5.html");
+			
+			bool inlist = false;
+			
+			
+			
+			try
+			{
+
+				using (StreamReader sr = new StreamReader("list2_5.html"))
+				{
+					String line;
+					// Read and display lines from the file until the end of
+					// the file is reached.
+					while ((line = sr.ReadLine()) != null)
+					{
+						if(line.IndexOf( "folder-list") != -1)inlist=true;
+						if(line.IndexOf("rightbanner")!= -1)break;
+						
+						if(inlist==true){
+							
+							if(line.IndexOf("title=")!= -1)
+							{
+								
+								if(line.IndexOf("http://fluffy.is/Files/")!= -1)
+								{
+									if(line.IndexOf("Folder Back")== -1)
+									{
+										string[] substrs = line.Split('\"');
+										//	statout(substrs[1]+" - "+substrs[3]);
+										
+										//	substrs[1]
+										activesubfolder = substrs[1];
+										parsefolder(((userData)comboBox1.SelectedItem).Value+"/"+activefolder+"/"+activesubfolder, true);
+										
+										
+									}
+									
+									//	statout(line);
+								}
+							}
+						}
+					}
+					
+				}
+			}
+			catch (Exception e)
+			{
+				// Let the user know what went wrong.
+				statout("The file could not be read:");
+				statout(e.Message);
+			}
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		public void downloadfolder(string furl)
+		{
+			downloadfile(furl, "list3.html");
+			int filenum=0;
+			int filetotal=0;
+			try
+			{
+
+				using (StreamReader sr = new StreamReader("list3.html"))
+				{
+					String line;
+					while ((line = sr.ReadLine()) != null)
+					{
+						if(line.IndexOf( "watch-") != -1)
+						{
+							filetotal++;
+							
+						}
+						
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				// Let the user know what went wrong.
+				statout("The file could not be read:");
+				statout(e.Message);
+			}
+			
+			try
+			{
+
+				using (StreamReader sr = new StreamReader("list3.html"))
+				{
+					String line;
+					// Read and display lines from the file until the end of
+					// the file is reached.
+					while ((line = sr.ReadLine()) != null)
+					{
+						if(line.IndexOf( "watch-") != -1)
+						{
+							//statout(line);
+							char[] delimiterChars = { '\"', '<', '>'};
+							string[] substr = line.Split(delimiterChars);
+							
+							filenum++;
+							label3.Text="File "+filenum+" of "+filetotal+"";
+							
+							downloadvideo(substr[2], substr[4]);
+							
+							
+							
+						}
+						
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				// Let the user know what went wrong.
+				statout("The file could not be read:");
+				statout(e.Message);
+			}
+		}
+		
+		
+		public bool isdownloading;
+		
+		public void downloadvideo(string furl, string fname)
+		{
+			string tempfolder = activefolder;
+			if(activesubfolder !="")tempfolder=activefolder+"\\"+activesubfolder;
+			
+			//	statout(activefolder +": Downloading "+fname+" from "+furl);
+			System.IO.Directory.CreateDirectory(tempfolder);
+			
+			if(File.Exists(tempfolder+"\\"+fname) == false)
+			{
+				
+
+				WebClient client = new WebClient();
+				client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+				client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+				
+				label1.Text=""+fname;
+				// Starts the download
+				isdownloading=true;
+				//   client.DownloadFileAsync(new Uri("http://fluffy.is/"+furl), activefolder+"\\"+fname);
+				client.DownloadFileAsync(new Uri("http://fluffy.is/"+furl), tempfolder+"\\"+furl);
+				
+				// client.DownloadFile(new Uri("http://fluffy.is/"+furl), activefolder+"\\"+fname);
+
+				//  btnStartDownload.Text = "Download In Process";
+				//  btnStartDownload.Enabled = false;
+
+				while(isdownloading)
+				{
+					Application.DoEvents();
+					System.Threading.Thread.Sleep(5);
+					
+				}
+				
+				System.IO.File.Move(tempfolder+"\\"+furl, tempfolder+"\\"+fname);
+
+			}
+			
+		}
+		
+		public double oldbytes=0;
+		public double bytedif =0;
+		public int oldsecs=0;
+		
+		void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+		{
+			double bytesIn = double.Parse(e.BytesReceived.ToString());
+			double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+			double percentage = bytesIn / totalBytes * 100;
+			
+			if(DateTime.Now.Second != oldsecs)
+			{
+				oldsecs = DateTime.Now.Second;
+				bytedif = bytesIn - oldbytes;
+				oldbytes=bytesIn;
+			}
+			
+			label4.Text = (int)(bytesIn/1024)+" KB / "+(int)(totalBytes/1024)+" KB at "+(int)(bytedif/1024)+" KB/s";
+
+			progressBar1.Value = int.Parse(Math.Truncate(percentage).ToString());
+		}
+
+
+		
+		void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+		{
+			//	MessageBox.Show("Download Completed");
+
+			isdownloading=false;
+			//  btnStartDownload.Text = "Start Download";
+			//  btnStartDownload.Enabled = true;
+		}
+
+		
+		
+		
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			comboBox1.Items.Add(new userData("Anime-A","http://fluffy.is/Files/Anime-A"));
+			comboBox1.Items.Add(new userData("Anime-E","http://fluffy.is/Files/Anime-E"));
+			comboBox1.Items.Add(new userData("Anime-Z","http://fluffy.is/Files/Anime-Z"));
+			comboBox1.Items.Add(new userData("Movies and OVA","http://fluffy.is/Files/Movies%20and%20OVA"));
+			comboBox1.Items.Add(new userData("Unfinished","http://fluffy.is/Files/Unfinished"));
+			
+			comboBox1.SelectedIndex = 0;
+			
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			string sourcelist = ((userData)comboBox1.SelectedItem).Value;
+			
+			downloadfile(sourcelist, "list1.html");
+			parseList("list1.html");
+			button2.Enabled = true;
+			groupBox2.Visible = true;
+			
+			
+		}
+		
+		public string activefolder;
+		public string activesubfolder;
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			
+			groupBox3.Visible = true;
+			button2.Enabled = false;
+			button1.Enabled = false;
+			comboBox1.Enabled = false;
+			checkedListBox1.Enabled = false;
+			
+			if(checkedListBox1.CheckedItems.Count != 0)
+			{
+				// If so, loop through all checked items and print results.
+				
+				for(int x = 0; x <= checkedListBox1.CheckedItems.Count - 1 ; x++)
+				{
+					activefolder = checkedListBox1.CheckedItems[x].ToString();
+					
+					label2.Text = "Anime "+(x+1)+" of "+checkedListBox1.CheckedItems.Count+" ("+activefolder+")";
+					
+					//statout(checkedListBox1.CheckedItems[x].ToString());
+					
+					parsefolder(((userData)comboBox1.SelectedItem).Value + "/"+activefolder, false);
+					
+				}
+
+			}
+		}
+		
+		void MainFormFormClosing(object sender, FormClosingEventArgs e)
+		{
+			Environment.Exit(0);
+		}
+	}
+}
